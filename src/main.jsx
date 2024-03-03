4ec6915456c2ceaa5753bfccfca0e3e79e7d7bea
grevy0810 <grrinconm@unal.edu.co>
2024-03-03 14:06:56 -0500

    installing tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Login from './components/Login/Login'
-//import App from './App.jsx'
-//import './index.css'
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Login></Login>
+    <App/>
   </React.StrictMode>,
 )
