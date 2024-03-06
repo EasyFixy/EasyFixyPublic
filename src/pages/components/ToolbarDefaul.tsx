8dc16bf847f243654afed86c6d8b6dc763f4c54e
grevy0810 <grrinconm@unal.edu.co>
2024-03-06 03:31:06 -0500

    diseño terminos y condiciones terminado

---
+++ src/pages/components/ToolbarDefaul.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+const ToolbarDefault = () =>{
+    return(
+        <div className="w-full h-24 backgroundVerde px-4 flex">
+            <div className="w-2/4 flex flex-row justify-start items-center">
+                <p className="text-3xl "> EasyFixy</p>
+                <img src="" alt="logo" className="w-[10%]" />
+
+            </div>
+            <div className="w-2/4 flex flex-row justify-end items-center">
+                <p className=" text-white">Ir a EasyFixy</p>
+
+            </div>
+
+        </div>
+    )
+
+}
+export default ToolbarDefault;
