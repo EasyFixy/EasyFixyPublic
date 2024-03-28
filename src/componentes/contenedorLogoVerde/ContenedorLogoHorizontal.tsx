601ce9c6322a7bf5de1ef092b7c6e6e932be0825
grevy0810 <grrinconm@unal.edu.co>
2024-03-27 23:21:11 -0500

    design crear empleo terminado

---
+++ src/componentes/contenedorLogoVerde/ContenedorLogoHorizontal.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Interface } from "readline";
+
+interface PropsContenedorLogo {
+    width?: string;
+}
+const ContenedorLogoHorizontal = ({width = "w-1/2"}:PropsContenedorLogo) =>{
+    return(
+        <div className={`h-full ${width} mainBackground flex justify-center items-center`}>
+            <div className="w-96 h-96">
+                <img src="/icons/icon.svg" alt="Icono principal" />
+            </div>
+        </div>
+    )
+}
+export default ContenedorLogoHorizontal;
