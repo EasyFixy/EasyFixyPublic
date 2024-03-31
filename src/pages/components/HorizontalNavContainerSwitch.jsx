73ded156ce659eb079d00b495908ccb08320a3c6
juan esteban <jcarranza@unal.edu.co>
2024-03-31 01:21:25 -0500

    cambios employer home

--- src/pages/components/HorizontalNavContainerSwitch.jsx
+++ src/pages/components/HorizontalNavContainerSwitch.jsx
@@ -4,15 +4,11 @@ import ToolbarDefault from "./ToolbarDefaul";
 const HorizontalNavContainerSwitch = (props) => {
     let content = props.content;
     const [seccionActiva, setSeccionActiva] = useState(0);
-    
-    const getClase = (i) => {
-        return "m-4 p-4 bg-gray-200"+ (i==seccionActiva ? "text-orange-500 underline": "")
-    }
 
     return (
         <>
             <div className="w-100">
-                <div class="flex">
+                <div className="flex">
                 {content.map((seccion, index) => (
                     <div className={index==seccionActiva ? "m-4 p-4 text-orange-500 underline": "m-4 p-4"} key={index} onClick={() => setSeccionActiva(index)}>
                         {seccion.name}
