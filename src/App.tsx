312ab64806bf003c7e948e5643f6c6107e304929
grevy0810 <grrinconm@unal.edu.co>
2024-04-01 01:23:18 -0500

    funcionalidad skills

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,16 @@ import UserLogueado from './Rutas/UserLogueado';
 import LandingPage from './pages/landingPage/LandingPage';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { validationToken } from './Helpers/Token';
+import { decodeJWT, validationToken } from './Helpers/Token';
+import { useAppDispatch } from './app/hooks';
+import { login } from './features/Auth/Auth';
 
 const App = () => {
   const tokenIsValid = validationToken();
+  const dispatch = useAppDispatch();
+  const token = localStorage.getItem('token');
+  const tokenDecode = decodeJWT();
+  dispatch(login({token:token ? token.toString(): '', id: tokenDecode.userId, date: tokenDecode.exp}))
   return (
     <>
       <BrowserRouter>
--- src/pages/Skills/Skills.tsx
+++ src/pages/Skills/Skills.tsx
@@ -2,18 +2,63 @@ import React, { useEffect, useState } from "react";
 import ToolbarDefault from "../components/ToolbarDefaul";
 import CreatableSelect from 'react-select/creatable';
 import { MultiValue } from "react-select";
+import { useAppSelector } from "../../app/hooks";
+import { handleRequestWithToken } from "../../Helpers/Request";
+import { useNavigate } from "react-router-dom";
 
 interface Skill {
     value: string;
     label: string;
 }
+interface requestData {
+    skills: string[],
+    token: string
+}
 
 const Skills = () => {
     const [selectedOption, setSelectedOption] = useState<MultiValue<Skill>>([]);
-
+    const navigate = useNavigate();
+    const token = useAppSelector(state => state.Auth.token)
     const handleChange = (newValue: MultiValue<Skill>) => {
+        console.log(newValue)
         setSelectedOption(newValue);
     };
+    const onClickButton = () =>{
+        handleRequestWithToken(handleRequest)
+    }
+    
+
+    const handleRequest = () => {
+        const selectedValues = selectedOption.map(option => option.value);
+        const requestData:requestData = {
+            skills: selectedValues,
+            token: token
+        };
+        // Opciones para la petición fetch
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestData) // Convertir el objeto a JSON
+        };
+        const url = 'http://localhost:3000/insertUserSkills';
+        fetch(url, requestOptions)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Response:', data);
+                navigate('/my/home/employee');
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                // Aquí puedes manejar el error como desees
+            });
+    }
 
     return(
         <div className="w-screen h-screen flex flex-col">
@@ -31,7 +76,10 @@ const Skills = () => {
                     placeholder="Añade habilidades..."
                 />
                 <hr className="border border-[#666666] border-dashed mb-12"/>
-                <button className="mainBackground self-end w-[185px] h-10 rounded-full text-white shadow-1">
+                <button 
+                    className="mainBackground self-end w-[185px] h-10 rounded-full text-white shadow-1"
+                    onClick={onClickButton}
+                >
                     Guardar
                 </button>
             </div>
